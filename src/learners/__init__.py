22f682a95cb614b172e844ac127b79a57f52d542
TERAO Hayato <terao@ist.hokudai.ac.jp>
2022-07-05 19:36:39 +0900

    update

---
+++ src/learners/__init__.py
@@ -0,0 +1,4 @@
+# flake8: noqa
+from .vat import VAT
+from .meanteacher import MeanTeacher
+from .pseudolabel import PseudoLabel
--- src/learners/meanteacher.py
+++ src/learners/meanteacher.py
@@ -1,20 +1,19 @@
 from __future__ import annotations
-from typing import Any, Callable
+from typing import Any
 
 import jax
 import jax.numpy as jnp
 import jax.random as jr
 from jax import nn
 from flax import core
-import chex
 
 from .learner import Learner, TrainState, functional as F, transforms as T
 
 Batch = Any
 
 
-class VAT(Learner):
-    """A Learner for Virtual Adversarial Training.
+class MeanTeacher(Learner):
+    """A Learner for Mean Teacher.
 
     Args:
         data_meta: Meta information of the dataset.
@@ -26,47 +25,13 @@ class VAT(Learner):
         precision (str): Precision. fp16, bf16 or fp32.
 
         lambda_y (float): Coefficient of the unsupervised loss.
-        lambda_entmin (float): Coefficient of the entropy loss.
         unsup_warmup_pos (float): Warmup position of epochs.
-        vat_eps (float): Norm of adversarial noises.
-        xi (float): Norm of adversarial noises to sample.
-        num_iters (int): Number of steps to update the adversarial noises.
     """
 
     lambda_y: float = 1.0
-    lambda_entmin: float = 0.0
     unsup_warmup_pos: float = 0.4
-    vat_eps: float = 6
 
-    xi: float = 1e-6
-    num_iters: int = 1
-
-    default_entries: list[str] = ["warmup", "loss", "ce_loss", "vat_loss", "entmin_loss", "acc1"]
-
-    def vat_loss(
-        self, rng: chex.PRNGKey, y: chex.Array, logits_y: chex.Array, apply_fn: Callable
-    ) -> chex.Array:
-        def grad_fn(z: chex.Array):
-            logits_yhat = apply_fn(y + z)
-            loss = F.kl_div(logits_y, nn.log_softmax(logits_yhat)).mean()
-            return loss
-
-        def normalize(x: chex.Array):
-            x /= jnp.linalg.norm(x, ord=2, axis=(-1, -2, -3), keepdims=True) + 1e-6
-            return x
-
-        def scan_fn(z: chex.Array, _):
-            z = self.xi * normalize(z)
-            z = grad_fn(z)
-            return z, _
-
-        z = jr.normal(rng, y.shape, dtype=y.dtype)
-        z = jax.lax.scan(scan_fn, z, jnp.arange(self.num_iters))
-        yhat = y + self.vat_eps * normalize(z)
-        logits_yhat = apply_fn(yhat)
-
-        vat_loss = F.kl_div(logits_yhat, nn.log_softmax(logits_y), log_targets=True).mean()
-        return vat_loss
+    default_entries: list[str] = ["warmup", "loss", "ce_loss", "mt_loss", "acc1"]
 
     def loss_fn(self, params: core.FrozenDict, train_state: TrainState, batch: Batch):
         rng, new_rng = jr.split(train_state.rng)
@@ -74,7 +39,7 @@ class VAT(Learner):
         warmup = jnp.clip(train_state.step / self.train_steps / self.unsup_warmup_pos, 0, 1)
         transform = jax.vmap(T.random_crop_flip(self.data_meta["image_size"]))
 
-        def apply_fn(x):
+        def apply_fn(x, params):
             variables = {"params": params, **train_state.model_state}
             output, new_model_state = train_state.apply_fn(
                 variables, x, train=True, mutable=["batch_stats"]
@@ -88,18 +53,18 @@ class VAT(Learner):
             batch["labeled"]["labels"], self.data_meta["num_classes"], self.label_smoothing
         )
 
-        rng, x_rng, y_rng = jr.split(rng, 3)
+        rng, x_rng, y_rng, yhat_rng = jr.split(rng, 4)
         x = transform(jr.split(x_rng, len(x)), x)
-        y = transform(jr.split(y_rng, len(y)), y)
+        y, yhat = transform(jr.split(y_rng, len(y)), y), transform(jr.split(yhat_rng, len(y)), y)
 
-        logits_x, new_model_state = apply_fn(x)
-        ce_loss = F.cross_entropy(logits_x, lx).mean()
+        logits_x, new_model_state = apply_fn(x, params)
+        ce_loss = F.cross_entropy(logits_x, lx)
 
-        logits_y, _ = apply_fn(y)
-        vat_loss = self.vat_loss(rng, y, logits_y, apply_fn)
-        entmin_loss = F.entropy(logits_y).mean()
+        logits_y, _ = apply_fn(y, params)
+        logits_yhat, _ = apply_fn(yhat, train_state.params_ema)
+        mt_loss = F.squared_error(nn.softmax(logits_y), nn.softmax(logits_yhat)).mean()
 
-        loss = ce_loss + self.lambda_y * warmup * vat_loss + self.lambda_entmin + entmin_loss
+        loss = ce_loss + self.lambda_y * warmup * mt_loss
         updates = {
             "model_state": new_model_state,
             "rng": new_rng,
@@ -107,8 +72,7 @@ class VAT(Learner):
         scalars = {
             "loss": loss,
             "ce_loss": ce_loss,
-            "vat_loss": vat_loss,
-            "entmin_loss": entmin_loss,
+            "mt_loss": mt_loss,
             "warmup": warmup,
             "acc1": F.accuracy(logits_x, lx, k=1),
             "acc5": F.accuracy(logits_x, lx, k=5),
