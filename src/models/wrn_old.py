e8709e661e2a84fc2b159f24f062fce87b99c6d9
TERAO Hayato <terao@ist.hokudai.ac.jp>
2022-07-10 12:28:16 +0900

    update.

--- src/models/wrn_old.py
+++ src/models/wrn_old.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
-from typing import Any, Callable
+from typing import Any
 import functools
-import math
 
 from jax import nn
 import jax.numpy as jnp
@@ -9,14 +8,14 @@ from flax import linen
 import chex
 from einops import reduce
 
+
 ModuleDef = Any
 
 
-class BasicBlock(linen.Module):
+class ResBlock(linen.Module):
 
     conv: ModuleDef
     norm: ModuleDef
-    act: Callable
 
     num_filters: int
     stride: int = 1
@@ -24,18 +23,19 @@ class BasicBlock(linen.Module):
 
     @linen.compact
     def __call__(self, x: chex.Array) -> chex.Array:
-        if x.shape[-1] != self.num_filters and self.activate_before_residual:
-            x = self.act(self.norm()(x))
-        else:
-            out = self.act(self.norm()(x))
-        if x.shape[-1] != self.num_filters:
-            out = x
-        out = self.conv(self.num_filters, (3, 3), self.stride)(out)
-        out = self.act(self.norm()(out))
-        out = self.conv(self.num_filters, (3, 3))(out)
-        if x.shape != out.shape:
+        h = x
+        if self.num_filters == x.shape[-1] or self.activate_before_residual:
+            h = nn.leaky_relu(self.norm()(h), 0.1)
+            if self.activate_before_residual:
+                x = h
+
+        h = self.conv(self.num_filters, (3, 3), self.stride)(h)
+        h = nn.leaky_relu(self.norm()(h), 0.1)
+        h = self.conv(self.num_filters, (3, 3))(h)
+        if x.shape != h.shape:
             x = self.conv(self.num_filters, (1, 1), self.stride)(x)
-        return out + x
+
+        return h + x
 
 
 class WideResNet(linen.Module):
@@ -47,15 +47,7 @@ class WideResNet(linen.Module):
     def __call__(self, x: chex.Array, train: bool = False) -> chex.Array:
         first_stride = x.shape[-2] > 128
 
-        conv = functools.partial(
-            linen.Conv,
-            kernel_init=nn.initializers.variance_scaling(
-                math.sqrt(2 / (1 + 0.01)),
-                "fan_out",
-                "normal",
-            ),
-            dtype=self.dtype,
-        )
+        conv = functools.partial(linen.Conv, dtype=self.dtype)
         norm = functools.partial(
             linen.BatchNorm,
             use_running_average=not train,
@@ -64,7 +56,6 @@ class WideResNet(linen.Module):
             dtype=self.bn_dtype,
             axis_name="batch",
         )
-        act = functools.partial(nn.leaky_relu, negative_slope=0.1)
 
         x = conv(16, (3, 3))(x)
         for i, block_size in enumerate(self.stage_sizes):
@@ -72,15 +63,14 @@ class WideResNet(linen.Module):
             for j in range(block_size):
                 stride = 2 if j == 0 and (i > 0 or first_stride) else 1
                 activate_before_residual = i == 0 and j == 0
-                x = BasicBlock(
+                x = ResBlock(
                     conv,
                     norm,
-                    act,
                     num_filters=num_filters,
                     stride=stride,
                     activate_before_residual=activate_before_residual,
                 )(x)
-        x = act(norm()(x))
+        x = nn.leaky_relu(norm()(x), 0.1)
         x = reduce(x, "B H W C -> B C", "mean")
         return x
 
